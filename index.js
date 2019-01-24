d08528c8347cad7e46afd69708ace971f80b4f51
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-24 03:42:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Chris'
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
